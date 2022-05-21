24ed2d0416aae3988f2306a393f0769410f31f6b
Kyle Laverty <k.laverty@gmail.com>
2022-05-21 16:01:27 -0400

    Added everything needed to have the player move around and bump into things on the default layer

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -26,17 +26,40 @@ public class PlayerController : MonoBehaviour
         // If movement input is not 0, try to move
         if(movementInput != Vector2.zero)
         {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset
-            );
+            bool success = TryMove(movementInput);
+
+            // If movement was not successful, try to move in only x direction.
+            if(!success){
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                // If x movement was not successful, try to move in only y direction.
+                if(!success){
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
         }
     }
 
     private void OnMove(InputValue movementValue)
     {
+        // Grab the input that was pressed and store for later processing.
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    private bool TryMove(Vector2 direction){
+        // Check for potential collisions.
+        int count = rb.Cast(
+            direction, // X and U vlaues between -1 and 1 that represent the direction from the body to look for collisions.
+            movementFilter, // The settings that determine where a collision can occur on such as layers to collide with.
+            castCollisions, // List of collisions to store and found collisions into after the Cast is finished.
+            moveSpeed * Time.fixedDeltaTime + collisionOffset // The amount to cast equal to the movement plus an offset.
+        );
+
+        if(count == 0){
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+
+        return false;
     }
 }
